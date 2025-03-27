26d1f59c6d663ce50e69661404a57537ac073046
Balazs777-hub <gbalazs22@gmail.com>
2025-03-27 15:58:00 +0100

    eslo

--- src/csigaverseny/CsigaVerseny.java
+++ src/csigaverseny/CsigaVerseny.java
@@ -10,31 +10,16 @@ public class CsigaVerseny {
     
     public static void main(String[] args) {
         
-//        Verseny verseny = new Verseny();    
-//
-//        System.out.println("Melyik csigára fogadsz? (Piros, Kék, Zöld)");
-//        String fogatSzin = scr.nextLine();
-//
-//        verseny.versenyIndito();
-//
-//        verseny.Fogadas(fogatSzin);
-//
-//        scr.close();
-            
-        Scanner scanner = new Scanner(System.in);
-        Verseny race = new Verseny();
+        Verseny verseny = new Verseny();    
 
-        // Fogadás kérdése
         System.out.println("Melyik csigára fogadsz? (Piros, Kék, Zöld)");
-        String betColor = scanner.nextLine();
+        String fogatSzin = scr.nextLine();
 
-        // Verseny indítása
-        race.startRace();
+        verseny.versenyIndito();
 
-        // Fogadás ellenőrzése
-        race.checkBet(betColor);
+        verseny.Fogadas(fogatSzin);
 
-        scanner.close();
+        scr.close();
 
     }
 
